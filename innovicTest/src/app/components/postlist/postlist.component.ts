052bae4813e5112780618df152c89ba1c29963a9
almirmerzic <33768032+almirmerzic@users.noreply.github.com>
2019-04-16 00:30:20 +0200

    Add postlist data
    Add postlist data

--- innovicTest/src/app/components/postlist/postlist.component.ts
+++ innovicTest/src/app/components/postlist/postlist.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-postlist',
@@ -7,9 +8,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PostlistComponent implements OnInit {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   ngOnInit() {
+    this.http.get("https://jsonplaceholder.typicode.com/posts")
+    .subscribe((data) => console.log(data));
   }
-
+  
 }
