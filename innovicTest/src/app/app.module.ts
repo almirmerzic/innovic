891a7bd36ddcaf78acaaf52e43a68c50917c6493
almirmerzic <33768032+almirmerzic@users.noreply.github.com>
2019-04-17 01:54:30 +0200

    Components Added and changed
    Components Added and changed

--- innovicTest/src/app/app.module.ts
+++ innovicTest/src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
-
+import { RouterModule} from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -9,6 +9,9 @@ import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { PostlistComponent } from './components/postlist/postlist.component';
 import { LatestcommentsComponent } from './components/latestcomments/latestcomments.component';
+import { AddUserComponent } from './components/add-user/add-user.component';
+import { PostViewComponent } from './components/post-view/post-view.component';
+import { PostcommentsComponent } from './components/postcomments/postcomments.component';
 
 @NgModule({
   declarations: [
@@ -16,12 +19,28 @@ import { LatestcommentsComponent } from './components/latestcomments/latestcomme
     HeaderComponent,
     FooterComponent,
     PostlistComponent,
-    LatestcommentsComponent
+    LatestcommentsComponent,
+    AddUserComponent,
+    PostViewComponent,
+    PostcommentsComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    RouterModule.forRoot([
+      {
+         path: 'postcomments-cmp',
+         component: PostcommentsComponent
+      }
+   ]),
+   RouterModule.forRoot([
+    {
+       path: 'postview-cmp',
+       component: PostViewComponent
+    }
+ ])
+
   ],
   providers: [],
   bootstrap: [AppComponent]
