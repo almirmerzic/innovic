3183a7f22db9c747e7b2dabc9e1c092d3eea914c
almirmerzic <33768032+almirmerzic@users.noreply.github.com>
2019-04-24 00:01:20 +0200

    Get data and show them on layout
    Get data and show them on layout

--- innovicTest/src/app/components/post-view/post-view.component.ts
+++ innovicTest/src/app/components/post-view/post-view.component.ts
@@ -1,22 +1,72 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { HttpClient } from '@angular/common/http';
+import { Posts } from "./models/posts";
+import { Users } from "./models/users";
+
+const postsEndpoint = 'https://jsonplaceholder.typicode.com/posts';
+const usersEndpoint = 'https://jsonplaceholder.typicode.com/users';
 
 @Component({
   selector: 'app-post-view',
   templateUrl: './post-view.component.html',
   styleUrls: ['./post-view.component.css']
 })
+
 export class PostViewComponent implements OnInit {
+
+  posts: [];
+  users: [];
+  comments: [];
+  post : {};
+  user : {};
+  comment:[];
 
   constructor(private route: ActivatedRoute, private http: HttpClient) {
   }
 
+
   ngOnInit() {
-    console.log(this.getPost());
+    this.getPostsandUsers();
   }
 
-  getPost(){
-    this.http.get("https://jsonplaceholder.typicode.com/posts")
+  getPostsandUsers = () => {
+
+    this.http.get(postsEndpoint)
+    .subscribe((data) => {
+      this.posts = data as any;
+
+      this.posts.forEach((p: Posts) => {
+        if (p.id == this.route.snapshot.params.id) {
+          this.post = p as any;
+          console.log('vo',this.post);
+        }
+      });
+    });
+
+    this.http.get(usersEndpoint)
+    .subscribe((data) =>  {
+      this.users = data as any;
+
+      this.users.forEach((p: Users) => {
+        if (p.id == this.route.snapshot.params.id) {
+          this.user = p as any;
+          console.log('vo',this.user);
+        }
+      });
+      console.log(this.users);
+    });
+
+    this.http.get(postsEndpoint)
+    .subscribe((data) => {
+      this.posts = data as any;
+
+      this.posts.forEach((p: Posts) => {
+        if (p.id == this.route.snapshot.params.id) {
+          this.post = p as any;
+          console.log('vo',this.post);
+        }
+      });
+    });
   }
 }
